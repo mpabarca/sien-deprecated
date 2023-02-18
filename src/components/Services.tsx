f07c467f694ab355e73506bf6782d16aeec286ec
Maria Paz Abarca <mp.abarca.alvear@gmail.com>
2023-02-18 01:57:58 +0100

    Add service component to home page

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -6,30 +6,36 @@ import Image from 'next/image'
 const Services = () => {
     return (
         <section className="container px-4 py-5" id="featured-3">
-            <div className="row g-4 py-5 row-cols-1 row-cols-lg-3">
-                <div className="feature col">
+            <div className="d-flex flex-column justify-content-center">
+                <div className="feature d-flex flex-row align-items-center justify-content-between m-auto">
                     <Image 
                         src={'/parking-animation-light.svg'}
                         alt='principal logo Sien services'
                         width={400}
                         height={200}
+                        className='col-4'
                     />
+                    <p className='col-6'>Servicio de Administración de Estacionamientos</p>
                 </div>
-                <div className="feature col">
+                <div className="feature d-flex flex-row align-items-center justify-content-between m-auto">
                     <Image 
                         src={'/computer-chart-animation.svg'}
                         alt='principal logo Sien services'
                         width={400}
                         height={200}
+                        className='col-4'
                     />
+                    <p className='col-6'>Servicio de Registro de Clientes e información de demanda</p>
                 </div>
-                <div className="feature col">
+                <div className="feature d-flex flex-row align-items-center justify-content-between m-auto">
                     <Image 
                         src={'/mobile-chart-animation.svg'}
                         alt='principal logo Sien services'
                         width={400}
                         height={200}
+                        className='col-4'
                     />
+                    <p className='col-6'>Impulso del negocio mediante promociones dirigidas</p>
                 </div>
             </div>
         </section>
